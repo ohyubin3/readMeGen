c6da86ec11dfcc982dbda721dad23f9a95d21a55
Sang Oh <baul3oh@gmail.com>
2022-09-22 22:11:43 -0500

    final save, did not know we had to make a repository

--- index.js
+++ index.js
@@ -1,71 +1,116 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+function renderLicenseBadge(license) {
+  let badgeString = "";
+  if (license === "MIT") {
+    badgeString = `[![MIT License](https://img.shields.io/badge/license-MIT-green.svg)](https://opensource.org/licenses/MIT)`;
+  } else if (license === "IBM") {
+    badgeString =
+      "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+  }
+  return badgeString;
+}
 
 inquirer
-    .prompt([
-        {
-            type:   'input',
-            name:   'title',
-            message: 'Please provide the title of your project.',
-        },
-        {
-            type:   'input',
-            name:   'Description',
-            message: 'Please provide your project description.',
-        },
-        {
-            type:   'input',
-            name:   'Installation',
-            message: 'Please provide your project installation instruction.',
-        },
-        {
-            type:   'input',
-            name:   'Usage',
-            message: 'Please provide your project usage information',
-        },
-        {
-            type:   'input',
-            name:   'guideline',
-            message: 'Please provide your project contribution guidelines',
-        },
-        {
-            type:   'input',
-            name:   'Tests',
-            message: '',
-        },   
-    ])
-    .then((response) => {
-        let readMeText = `
-        ## Title
-        ${}
-        
-        
-        
-        
-        
-        `
+  .prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "Please provide the title of your project.",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please provide your project description.",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Please provide your project installation instructions.",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Please provide your project usage information",
+    },
+    {
+      type: "input",
+      name: "guideline",
+      message: "Please provide your project contribution guidelines.",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Please choose your licenses.",
+      choices: ["MIT", "IBM"],
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "Please provide your test instructions for your project.",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Please provide your github repository address",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Please provide your email",
+    },
+  ])
+  .then((response) => {
+    let readMeText = `
+# ${renderLicenseBadge(response.license)}
 
+# ${response.title}
 
-    }
-
-
-
-
-    // .then((response) => {
-    //     console.log(response)
-    //     fs.watchFile("newReadMe.md", readMeText(answers), function (err) {
-    //         if (err) console.log(err);
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributors](#contributors)
+* [License](#license)
+* [Test](#test)
+* [Contact Me](#contact)
     
-            
-        
+# Description
+    ${response.description}
+    
+
+
+# Installation
+    ${response.installation}
+    
+
+
+# Usage
+    ${response.usage}
+    
+
+
+# Contributors
+    ${response.guideline}
+    
+
+
+# License 
+    ◈This project has been covered under the ${response.license} license.
+    
+
+
+# Test
+    ${response.test}
 
 
 
-// fs.writeFile("read.md", readmeText, function (err) {
-
-//     if (err)
-//         console.log(err);
-
-// })
-// });
+# Contact Me
+    Github : https://github.com/${response.github}
+    Email : ${response.email}
+`;
+    fs.writeFile("newReadMe.md", readMeText, function (err) {
+      if (err) console.log(err);
+    });
+  });
---
+++ newReadMe.md
@@ -0,0 +1,47 @@
+
+# [![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)
+
+# ReadMe Generator
+
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributors](#contributors)
+* [License](#license)
+* [Test](#test)
+* [Contact Me](#contact)
+    
+# Description
+    This is where description will go
+    
+
+
+# Installation
+    Installation instructions
+    
+
+
+# Usage
+    How to use
+    
+
+
+# Contributors
+    Who will you include for contributors
+    
+
+
+# License 
+    ◈This project has been covered under the IBM license.
+    
+
+
+# Test
+    How to test this app
+
+
+
+# Contact Me
+    Github : https://github.com/ohyubin3
+    Email : baul3oh@gmail.com
